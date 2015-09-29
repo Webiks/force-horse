c892929f41999a2b1bee8ea0ceab24aa0b8e85a7
yccteam <kra.yonatan@gmail.com>
2015-09-29 06:32:01 +0300

    Added ngCesiumClustering
    With tests. Need to finalize the "cluster" method and the documentation

--- src/app/components/ngCesium/ngCesiumClustering/ngCesiumClustering.js
+++ src/app/components/ngCesium/ngCesiumClustering/ngCesiumClustering.js
@@ -1,30 +1,194 @@
 // define the ngCesiumFilter module (dependant on ngCesium)
 angular.module('ngCesiumClustering', ['ngCesium'])
-    .directive('cesiumCluster', function(cesiumClusteringFactory){
+/**
+ * @name cesiumClustering
+ * @attrs cesiumClustering -> holds the groups definitions:
+ * {
+     *      defaultRadius: {float},
+     *      dataSource: {*}, // can be either {int} (index of ds) or {string} (name of ds). if empty, uses the viewer's entities
+     *      groups: {
+     *          radius: {float}, // clustering radius of the group, in pixels (TODO::allow for meters)
+     *          name: {string}, // group name
+     *          color: {string},// group color (if none given, would be taken from cesiumClusteringConstants.colors)
+     *          property: {*} // an entity's property to group by TODO::enable as a function for flexibility
+     *      }
+     * }
+ */
+    .directive('cesiumClustering', ['cesiumClusteringFactory', function (cesiumClusteringFactory) {
         return {
             restrict: 'A',
             require: 'cesiumDirective',
-            link: function(scope, element, attr, ctrl){
-                // create an isolate scope for this directive
-                var isoScope = scope.$new(true);
-                // add parent's controller to the scope
-                isoScope.cesiumFilterCtrl = ctrl;
-                // create a cesiumButtonsInstance and add it to our controller
-                isoScope.cesiumFilterCtrl.cesiumFilterInstance = new cesiumFilterFactory(ctrl.cesiumDirective.cesiumInstance);
+            link: function (scope, element, attrs, ctrl) {
+
+                var options = scope.$eval(attrs.cesiumClustering);
+
+                ctrl.cesiumClusteringInstance = new cesiumClusteringFactory(ctrl.cesiumDirective.cesiumInstance, options);
             }
         };
+    }])
+    .factory('cesiumClusteringFactory', ['$rootScope', 'cesiumClusteringConstants', 'cesiumClusteringGroup',
+        function ($rootScope, cesiumClusteringConstants, cesiumClusteringGroup) {
+            // constructor
+            function cesiumClusteringFactory(ngCesiumInstance, config) {
+                var that = this;
+                that.groups = [];
+                that.ngCesiumInstance = ngCesiumInstance;
+                ngCesiumInstance.cesiumClustering = that;
+                that.refreshConfig(config);
+            }
+
+            cesiumClusteringFactory.prototype = {
+                /**
+                 * @name cluster
+                 * @description iterates over the groups and runs the cluster function for each group
+                 */
+                cluster: function cluster() {
+                    var that = this;
+
+                    for (var i = 0; i < that.groups.length; i++) {
+                        that.clusterGroup(that.groups[i]);
+                    }
+                },
+                /**
+                 * @name clusterGroup
+                 * @param groupData
+                 * @description cluster the entities in each group
+                 */
+                clusterGroup: function clusterGroup(groupData) {
+                    var that = this;
+
+
+                },
+                /**
+                 * @name setGroups
+                 * @description Divides the entities in the viewer to groups according to config. Entities that are in a group become hidden, while those that don't fit any group, remain shown.
+                 * @returns {*} false for error, the groups array for success
+                 */
+                setGroups: function setGroups() {
+                    var that = this;
+                    if (angular.isUndefined(that.config)) {
+                        return false;
+                    }
+
+                    // clear the groups
+                    that.groups.length = 0;
+
+                    // create the groups anew from config
+                    for (var i = 0; i < that.config.groups.length; i++) {
+                        that.createGroup(that.config.groups[i]);
+                    }
+
+                    // now for every entity, set it in a group
+                    var entities = that.ngCesiumInstance._viewer.entities.values;
+                    for (i = 0; i < entities.length; i++) {
+                        that.setInGroup(entities[i]);
+                    }
+
+                    return that.groups;
+
+                },
+                /**
+                 * @name createGroup
+                 * @param group
+                 * @description gets a group config and adds it to the list of groups in the instance
+                 */
+                createGroup: function createGroup(group) {
+                    var that = this;
+
+                    that.groups.push(new cesiumClusteringGroup(group, that.config));
+                },
+                /**
+                 * @name setInGroup
+                 * @param entity
+                 * @description checks the entity vs. the groups and adds the entity to the group it belongs to (the first found).
+                 * if it is indeed in a group, it "show" would be set to false.
+                 */
+                setInGroup: function setInGroup(entity) {
+                    var that = this;
+                    var propertyName, propertyValue;
+                    for (var i = 0; i < that.groups.length; i++) {
+                        // TODO::complete the "setInGroup" logic
+                        propertyName = angular.isFunction(that.groups[i].property.name) ? that.groups[i].property.name() : that.groups[i].property.name;
+                        propertyValue = angular.isFunction(that.groups[i].property.value) ? that.groups[i].property.value() : that.groups[i].property.value;
+
+                        if ($rootScope.$eval(propertyName, entity) === propertyValue) {
+                            that.groups[i].dataSource.entities.add(entity);
+                            entity.show = false;
+                            return i;
+                        }
+                    }
+                    return -1;
+                },
+                /**
+                 * @name setConfig
+                 * @param config
+                 * @description sets the config to a new config. Doesn't update the view immediately. In order to do that use "refreshConfig"
+                 */
+                setConfig: function setConfig(config) {
+                    // TODO::config validations
+                    var that = this;
+                    that.config = config;
+                },
+                /**
+                 * @name refreshConfig
+                 * @param config
+                 * @description gets a config object, sets it, parses it (divides into groups etc.) and then reruns the clustering
+                 */
+                refreshConfig: function refreshConfig(config) {
+                    var that = this;
+                    that.setConfig(config);
+                    // TODO::refresh the data
+
+                    // divide into groups again
+                    that.setGroups();
+
+                    // cluster anew
+                    that.cluster();
+                }
+            };
+
+            return cesiumClusteringFactory;
+        }])
+
+    .constant('cesiumClusteringConstants', {
+        /**
+         * holds 8 colors for 8 groups. If there are more than 8 groups without colors, colors will be repeated.
+         * current holds the current color that is supposed to be chosen for the group
+         */
+        groupsColors: {
+            colorsList: ['#C259C2', '#62B050', '#530C40', '#9E8C48', '#1B4D11', '#563D37', '#84959B', '#C74645', '#2F72BF', '#F6A050'],
+            current: 0,
+            /**
+             *
+             * @returns {*}
+             */
+            pickColor: function () {
+                var that = this;
+
+                if (that.current >= that.colorsList.length) {
+                    that.current = 0;
+                }
+
+                return that.colorsList[that.current++];
+            }
+        }
     })
-    .factory('cesiumClusteringFactory', function($q){
+
+    .factory('cesiumClusteringGroup', ['cesiumClusteringConstants',
+        function (cesiumClusteringConstants) {
         // constructor
-        function cesiumClusteringFactory(ngCesiumInstance){
+        function cesiumClusteringGroup(group, config) {
+            // TODO::validate group config
             var that = this;
-            that.ngCesiumInstance = ngCesiumInstance;
-            ngCesiumInstance.cesiumClusterer = that;
+            this.color = group.color ? group.color : cesiumClusteringConstants.groupsColors.pickColor();
+            this.name = group.name;
+            this.id = Math.random().toString(36).substring(7); //unique id...
+            this.dataSource = new Cesium.CustomDataSource(this.id);
+            this.radius = group.radius ? group.radius : config.defaultRadius;
+            this.property = group.property;
+
+            return this;
         }
 
-        cesiumClusteringFactory.prototype = {
-
-        };
-
-        return cesiumClusteringFactory;
-    });
+        return cesiumClusteringGroup;
+    }]);
--- src/app/components/ngCesium/ngCesiumClustering/ngCesiumClustering_test.js
+++ src/app/components/ngCesium/ngCesiumClustering/ngCesiumClustering_test.js
@@ -1,30 +1,394 @@
 'use strict';
+describe('ngCesium Clustering module tests', function () {
 
-xdescribe('ngCesium Clustering module tests', function() {
+    var options = {
+        extensionName: 'ngCesiumClustering',
+        extensionConfig: {
+            propertyInScope: 'cesiumConfig.clusterConfig',
+            propertyContent: {
+                groups: [
+                    // standart
+                    {
+                        name: 'group1',
+                        color: '#654EA0',
+                        property: {
+                            name: 'type',
+                            value: 'pizzeria'
+                        }
+                    },
+                    // group value as a function
+                    {
+                        name: 'group2',
+                        color: '#111AE9',
+                        property: {
+                            name: 'type',
+                            value: function (entity) {
+                                return entity.position.x > 10;
+                            }
+                        }
+                    },
+                    // no color
+                    {
+                        name: 'group3',
+                        property: {
+                            name: 'type',
+                            value: 'group3'
+                        }
+                    }
 
-    var $compile, $rootScope, element, ngCesiumClustering;
-    beforeEach(module('ngCesiumClustering'));
+                ]
+            }
+        }
+    };
 
-    beforeEach(inject(function(_$compile_, _$rootScope_, _ngCesiumClustering_) {
-        $compile = _$compile_;
-        $rootScope = _$rootScope_;
-        ngCesiumClustering = _ngCesiumClustering_;
-        $rootScope.cesiumConfig = {};
-        element = $compile('<div cesium-directive="cesiumConfig" cesium-add-remove-buttons-directive></div>')($rootScope);
-    }));
+    ngCesiumExtensionTest(options);
 
-    describe('ngCesiumClustering directive tests', function() {
-        it('ngCesiumClustering should be created with the cesium instance', function(){
-            var isoScope = element.scope().$$childHead;
-            expect(isoScope.cesiumCtrl.cesiumDirective.cesiumInstance.cesiumClusterer).toBeDefined();
-        });
+    /*beforeEach(module('ngCesiumClustering'));
+
+     beforeEach(inject(function(_$compile_, _$rootScope_, _cesiumClusteringFactory_) {
+     $compile = _$compile_;
+     $rootScope = _$rootScope_;
+     cesiumClusteringFactory = _cesiumClusteringFactory_;
+     $rootScope.cesiumConfig = {
+     config: {
+     baseLayerPicker: false,
+     fullscreenButton: false,
+     homeButton: false,
+     sceneModePicker: false,
+     selectionIndicator: false,
+     timeline: false,
+     animation: false,
+     geocoder: false
+     }
+     };
+     element = $compile('<div cesium-directive="cesiumConfig" cesium-clustering></div>')($rootScope);
+     isoScope = element.scope().$$childHead;
+     cesiumClustering = isoScope.cesiumCtrl.cesiumDirective.cesiumInstance.cesiumClustering;
+     }));*/
+
+    describe('ngCesiumClustering directive tests', function () {
+
     });
 
-    describe('ngCesiumClustering factory tests', function() {
-        it('ngCesiumClustering should be in the cesium instance', function(){
-            var isoScope = element.scope().$$childHead;
-            expect(isoScope.cesiumCtrl.cesiumDirective.cesiumInstance.cesiumClusterer).toBeDefined();
+    describe('ngCesiumClustering factory tests', function () {
+        it('ngCesiumClustering should be in the cesium instance', function () {
+            expect(options.isoScope.cesiumCtrl.cesiumDirective.cesiumInstance.cesiumClustering).toBeDefined();
         });
+
+        describe('clusterGroup(config) tests', function () {
+            it('clusterGroup should start the clustering methods', function () {
+
+            });
+        });
+
+        describe('cluster() tests', function () {
+            it('cluster should call clusterGroup for all existing groups', function () {
+                options.extensionInstance.groups = [1, 2];
+                spyOn(options.extensionInstance, 'clusterGroup');
+                options.extensionInstance.cluster();
+                expect(options.extensionInstance.clusterGroup).toHaveBeenCalledWith(1);
+                expect(options.extensionInstance.clusterGroup).toHaveBeenCalledWith(2);
+            });
+        });
+
+        describe('refreshConfig(config) tests', function () {
+            var config = {
+                defaultRadius: 100,
+                dataSource: undefined,
+                groups: [
+                    // standart
+                    {
+                        name: 'group1',
+                        color: '#654EA0',
+                        property: {
+                            name: 'type',
+                            value: 'pizzeria'
+                        }
+                    },
+                    // group value as a function
+                    {
+                        name: 'group2',
+                        color: '#111AE9',
+                        property: {
+                            name: 'type',
+                            value: function (entity) {
+                                return entity.position.x > 10;
+                            }
+                        }
+                    },
+                    // no color
+                    {
+                        name: 'group3',
+                        property: {
+                            name: 'type',
+                            value: 'group3'
+                        }
+                    }
+
+                ]
+            };
+
+            it('should call setConfig(config)', function () {
+                spyOn(options.extensionInstance, 'setConfig');
+                options.extensionInstance.refreshConfig(config);
+                expect(options.extensionInstance.setConfig).toHaveBeenCalledWith(config);
+            });
+
+            it('should call setGroups()', function () {
+                spyOn(options.extensionInstance, 'setGroups');
+                options.extensionInstance.refreshConfig(config);
+                expect(options.extensionInstance.setGroups).toHaveBeenCalled();
+            });
+
+            it('should call cluster()', function () {
+                spyOn(options.extensionInstance, 'cluster');
+                options.extensionInstance.refreshConfig(config);
+                expect(options.extensionInstance.cluster).toHaveBeenCalled();
+            });
+        });
+
+        describe('setGroups() tests', function () {
+
+            function setData() {
+                config = options.extensionInstance.config = {
+                    defaultRadius: 100,
+                    dataSource: undefined,
+                    groups: [
+                        // standart
+                        {
+                            name: 'group1',
+                            color: '#654EA0',
+                            property: {
+                                name: 'type',
+                                value: 'pizzeria'
+                            }
+                        },
+                        // group value as a function
+                        {
+                            name: 'group2',
+                            color: '#111AE9',
+                            property: {
+                                name: 'type',
+                                value: 'burgers bar'
+                            }
+                        },
+                        // no color
+                        {
+                            name: 'group3',
+                            property: {
+                                name: 'type',
+                                value: 'group3'
+                            }
+                        }
+                    ]
+                };
+                viewer = options.extensionInstance.ngCesiumInstance._viewer;
+                // add entities - 5 groups, with 10 elements each for
+                var i, j, entitySpecs = {};
+                for (i = 0; i < config.groups.length; i++) {
+                    entitySpecs[config.groups[i].property.name] = config.groups[i].property.value;
+                    for (j = 0; j < 10; j++) {
+                        viewer.entities.add(entitySpecs);
+                    }
+                }
+            }
+
+            var config, viewer;
+
+            // set the data
+            beforeEach(function () {
+                setData();
+            });
+
+            // clear data after an "it"
+            afterEach(function () {
+                viewer.entities.removeAll();
+                options.extensionInstance.groups.length = 0;
+            });
+
+            it('should return false if config does not exist', function () {
+                options.extensionInstance.config = undefined;
+                expect(options.extensionInstance.setGroups()).toBe(false);
+            });
+
+            it('should call createGroup()', function () {
+                spyOn(options.extensionInstance, 'createGroup');
+                options.extensionInstance.setGroups();
+                expect(options.extensionInstance.createGroup.calls.count()).toEqual(3);
+            });
+
+            it('should call setInGroup(entity)', function () {
+                spyOn(options.extensionInstance, 'setInGroup');
+                options.extensionInstance.setGroups();
+                expect(options.extensionInstance.setInGroup.calls.count()).toEqual(30);
+            });
+
+            it('should return the groups if success', function () {
+                var expectedOutcome = options.extensionInstance.setGroups();
+                expect(expectedOutcome.length).toBe(3);
+                expect(expectedOutcome[0].dataSource.entities.values.length).toBe(10);
+                expect(expectedOutcome[1].dataSource.entities.values.length).toBe(10);
+                expect(expectedOutcome[2].dataSource.entities.values.length).toBe(10);
+            });
+        });
+
+        describe('clusterGroup(group) tests', function(){
+           //TODO::fill this up!
+        });
+
+        it('setConfig(config) should alter the config', function () {
+            var config = {
+                propertyInScope: 'cesiumConfig.clusterConfig',
+                propertyContent: {
+                    defaultRadius: 100,
+                    dataSource: undefined,
+                    groups: [
+                        // standart
+                        {
+                            name: 'group1',
+                            color: '#654EA0',
+                            property: {
+                                name: 'type',
+                                value: 'pizzeria'
+                            }
+                        },
+                        // group value as a function
+                        {
+                            name: 'group2',
+                            color: '#111AE9',
+                            property: {
+                                name: 'type',
+                                value: function (entity) {
+                                    return entity.position.x > 10;
+                                }
+                            }
+                        },
+                        // no color
+                        {
+                            name: 'group3',
+                            property: {
+                                name: 'type',
+                                value: 'group3'
+                            }
+                        }
+
+                    ]
+                }
+            };
+            options.extensionInstance.setConfig(config);
+            expect(options.extensionInstance.config).toEqual(config);
+        });
+
+    });
+
+    describe('ngCesiumClustering constant tests', function () {
+        var clusteringConstans;
+        beforeEach(inject(function (_cesiumClusteringConstants_) {
+            clusteringConstans = _cesiumClusteringConstants_;
+        }));
+
+        describe('groupsColors tests', function () {
+            var groupsColors;
+            beforeEach(function () {
+                groupsColors = clusteringConstans.groupsColors;
+                groupsColors.current = 0;
+            });
+
+            it('pickColor should return the first color in the list', function () {
+                var pickedColor = groupsColors.colorsList[0];
+                expect(groupsColors.pickColor()).toEqual(pickedColor);
+            });
+
+            it('after choosing a color, the current should be 1', function () {
+                groupsColors.pickColor();
+                expect(groupsColors.current).toEqual(1);
+            });
+
+            it('after choosing a color when current is at max, the current should be 1 and the first color should be returned', function () {
+                var expectedPickedColor = groupsColors.colorsList[0];
+                groupsColors.current = groupsColors.colorsList.length;
+                expect(groupsColors.pickColor()).toEqual(expectedPickedColor);
+                expect(groupsColors.current).toEqual(1);
+            });
+        });
+
+
+    });
+
+    describe('cesiumClusteringGroup tests', function () {
+        var cesiumClusteringGroup;
+        var config = {
+            defaultRadius: 100,
+            dataSource: undefined,
+            groups: [
+                // standart
+                {
+                    name: 'group1',
+                    color: '#654EA0',
+                    property: {
+                        name: 'type',
+                        value: 'pizzeria'
+                    }
+                },
+                // group value as a function
+                {
+                    name: 'group2',
+                    color: '#111AE9',
+                    property: {
+                        name: 'type',
+                        value: function (entity) {
+                            return entity.position.x > 10;
+                        }
+                    }
+                },
+                // no color
+                {
+                    name: 'group3',
+                    property: {
+                        name: 'type',
+                        value: 'group3'
+                    }
+                }
+
+            ]
+
+        };
+        var result;
+        beforeEach(inject(function (_cesiumClusteringGroup_) {
+            cesiumClusteringGroup = _cesiumClusteringGroup_;
+            result = new cesiumClusteringGroup(config.groups[0], config);
+        }));
+
+        describe('should create a group instance', function () {
+
+            it('should set the name according to group name', function () {
+                expect(result.name).toEqual('group1');
+            });
+
+            it('should set the color according to group color', function () {
+                expect(result.color).toEqual('#654EA0');
+            });
+
+            it('should set a random id', function () {
+                expect(result.id).toBeDefined();
+            });
+
+            it('should set a data source', function () {
+                expect(result.dataSource).toBeDefined();
+            });
+
+            it('should set the radius according to defaults', function () {
+                expect(result.radius).toEqual(100);
+            });
+
+            it('should set the radius according to group radius', function () {
+                config.groups[0].radius = 50;
+                result = new cesiumClusteringGroup(config.groups[0], config);
+                expect(result.radius).toEqual(50);
+            });
+
+
+        })
 
     });
 });
