c2ecd71e18b8e4cc3d0d106e157db1b7b56c9838
ramtob <ramtob@gmail.com>
2016-01-24 11:54:11 +0200

    Replaced references to $scope with references to the controller

--- src/app/views/viewAutoForceLayout/viewAutoForceLayout.js
+++ src/app/views/viewAutoForceLayout/viewAutoForceLayout.js
@@ -8,7 +8,7 @@ angular.module('viewAutoForceLayout', ['ui.router', 'autoForceLayout'])
         $stateProvider.state('index.viewAutoForceLayout', {
             url: '/viewAutoForceLayout',
             templateUrl: 'app/views/viewAutoForceLayout/viewAutoForceLayout.html',
-            controller: 'view3Ctrl as viewCtrl',
+            controller: 'view3Ctrl as ctrl',
             data: {
                 title: 'Auto Force Layout'
             }
@@ -16,36 +16,37 @@ angular.module('viewAutoForceLayout', ['ui.router', 'autoForceLayout'])
     }])
 
     //---------------------------------------------------------------//
-    .controller('view3Ctrl', ['$scope', '$http', 'graphData', 'ViewAutoForceLayoutConstants', function ($scope, $http, graphData, constants) {
+    .controller('view3Ctrl', ['$http', 'graphData', 'ViewAutoForceLayoutConstants', function ($http, graphData, constants) {
         //console.log('In view3Ctrl');
-
-        $scope.options = {};
-        $scope.options.data = graphData.getRandomData($scope.numOfNodes = constants.INITIAL_NUM_OF_NODES);
-        $scope.setArrays = function () {
-            $scope.data = [];
-            $scope.data[constants.NODES] = $scope.options.data[constants.NODES].data;
-            $scope.data[constants.EDGES] = $scope.options.data[constants.EDGES].data;
+        var vm = this;
+        
+        vm.options = {};
+        vm.options.data = graphData.getRandomData(vm.numOfNodes = constants.INITIAL_NUM_OF_NODES);
+        vm.setArrays = function () {
+            vm.data = [];
+            vm.data[constants.NODES] = vm.options.data[constants.NODES].data;
+            vm.data[constants.EDGES] = vm.options.data[constants.EDGES].data;
         };
-        $scope.setArrays();
-        $scope.NODES = constants.NODES;
-        $scope.EDGES = constants.EDGES;
+        vm.setArrays();
+        vm.NODES = constants.NODES;
+        vm.EDGES = constants.EDGES;
 
-        $scope.createRandomGraph = function () {
-            $scope.options.data = graphData.getRandomData($scope.numOfNodes);
-            $scope.setArrays();
-            $scope.options.autoForceLayoutInstance.redraw();
+        vm.createRandomGraph = function () {
+            vm.options.data = graphData.getRandomData(vm.numOfNodes);
+            vm.setArrays();
+            vm.options.autoForceLayoutInstance.redraw();
         };
 
-        $scope.createGraphFromFile = function () {
+        vm.createGraphFromFile = function () {
             var f = event.target.files[0];
             if (f) {
                 var r = new FileReader();
                 r.onload = function(evt2) {
                     var data = JSON.parse(evt2.target.result);
-                    $scope.options.data = graphData.getDataFromFile(data);
-                    $scope.setArrays();
-                    $scope.numOfNodes = $scope.data[constants.NODES].length; // show no. of nodes on screen
-                    $scope.options.autoForceLayoutInstance.redraw();
+                    vm.options.data = graphData.getDataFromFile(data);
+                    vm.setArrays();
+                    vm.numOfNodes = vm.data[constants.NODES].length; // show no. of nodes on screen
+                    vm.options.autoForceLayoutInstance.redraw();
                 };
                 r.readAsText(f);
             } else {
@@ -53,100 +54,100 @@ angular.module('viewAutoForceLayout', ['ui.router', 'autoForceLayout'])
             }
         };
 
-        //$scope.graphDataFileName = "";
-        //$scope.createGraphFromFile = function () {
-        //    $http.get($scope.graphDataFileName)
+        //vm.graphDataFileName = "";
+        //vm.createGraphFromFile = function () {
+        //    $http.get(vm.graphDataFileName)
         //        .then(function (response) {
-        //            $scope.options.data = graphData.getDataFromFile(response.data);
-        //            $scope.setArrays();
-        //            $scope.numOfNodes = $scope.data[constants.NODES].length; // show no. of nodes on screen
-        //            $scope.options.autoForceLayoutInstance.redraw();
+        //            vm.options.data = graphData.getDataFromFile(response.data);
+        //            vm.setArrays();
+        //            vm.numOfNodes = vm.data[constants.NODES].length; // show no. of nodes on screen
+        //            vm.options.autoForceLayoutInstance.redraw();
         //        },
         //        function(response) {
         //            console.warn(`File read error: status = ${response.status}, message = ${response.statusText}`);
         //        });
         //};
 
-        $scope.selectedItems = [new Set(), new Set()]; // selected nodes, edges
+        vm.selectedItems = [new Set(), new Set()]; // selected nodes, edges
 
         //----- Event handlers -----//
 
         // An element was hovered inside this view
-        $scope.onHoverInside = function (item, on) {
+        vm.onHoverInside = function (item, on) {
             item.hovered = on;
-            $scope.setHoverState(item);
-            if (angular.isDefined($scope.options.autoForceLayoutInstance)) {
-                $scope.options.autoForceLayoutInstance.onHoverOutside(item);
+            vm.setHoverState(item);
+            if (angular.isDefined(vm.options.autoForceLayoutInstance)) {
+                vm.options.autoForceLayoutInstance.onHoverOutside(item);
             }
         };
 
         // An element was hovered outside this view (in the graph component)
-        $scope.onHoverOutside = function (item) {
-            $scope.setHoverState(item);
+        vm.onHoverOutside = function (item) {
+            vm.setHoverState(item);
         };
 
         // Update hover-related fields
-        $scope.setHoverState = function (item) {
+        vm.setHoverState = function (item) {
             if (item) {
                 if (item.hovered) {
                     if (item.class === constants.CLASS_NODE) {
-                        $scope.lastHoveredNode = item;
+                        vm.lastHoveredNode = item;
                     } else if (item.class === constants.CLASS_EDGE) {
-                        $scope.lastHoveredEdge = item;
+                        vm.lastHoveredEdge = item;
                     }
                 }
             }
         };
 
         // An element was clicked on
-        $scope.onClick = function (event, item) {
+        vm.onClick = function (event, item) {
             var element = event.currentTarget;
             // If the Ctrl key was pressed during the click ..
             // If the clicked element was marked as selected, unselect it, and vice versa
             if (event.ctrlKey) {
-                $scope.onSelectInside(element, item, !item.selected);
+                vm.onSelectInside(element, item, !item.selected);
             } else {
                 // If the Ctrl key was not pressed ..
                 // If the clicked node is selected, ignore the click
                 // Else, clear the current selection, and select the clicked node
                 if (!item.selected) {
-                    $scope.onSelectInside(element, item, true, true);
+                    vm.onSelectInside(element, item, true, true);
                 }
             }
         };
 
         // Elements were selected inside this view
-        $scope.onSelectInside = function (element, item, on, clearOldSelection) {
+        vm.onSelectInside = function (element, item, on, clearOldSelection) {
             var itemType = (item.class === constants.CLASS_NODE ? constants.NODES : constants.EDGES);
 
             if (clearOldSelection) {
-                $scope.data[itemType].filter(function (d) {
-                    return $scope.selectedItems[itemType].has(d.id);
+                vm.data[itemType].filter(function (d) {
+                    return vm.selectedItems[itemType].has(d.id);
                 }).forEach(function (d) {
                     d.selected = false;
                 });
-                $scope.selectedItems[itemType].clear();
+                vm.selectedItems[itemType].clear();
             }
 
             // Update the selectedItems set
             if (item.selected = on) {
-                $scope.selectedItems[itemType].add(item.id);
+                vm.selectedItems[itemType].add(item.id);
             } else {
-                $scope.selectedItems[itemType].delete(item.id);
+                vm.selectedItems[itemType].delete(item.id);
             }
 
-            if (angular.isDefined($scope.options.autoForceLayoutInstance)) {
-                $scope.options.autoForceLayoutInstance.onSelectOutside();
+            if (angular.isDefined(vm.options.autoForceLayoutInstance)) {
+                vm.options.autoForceLayoutInstance.onSelectOutside();
             }
         };
 
         // Elements were selected and/or unselected somewhere
-        $scope.onSelectOutside = function () {
+        vm.onSelectOutside = function () {
             for (var itemType = constants.NODES; itemType <= constants.EDGES; itemType++) {
-                $scope.selectedItems[itemType].clear();
-                $scope.data[itemType].forEach(function (item) {
+                vm.selectedItems[itemType].clear();
+                vm.data[itemType].forEach(function (item) {
                     if (item.selected) {
-                        $scope.selectedItems[itemType].add(item.id);
+                        vm.selectedItems[itemType].add(item.id);
                     }
                 });
             }
